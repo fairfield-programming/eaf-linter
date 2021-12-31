06c50a383fc964e2df401ca876365517bf4cdbc1
William McGonagle <wpmcgonagle@gmail.com>
2021-12-31 00:01:51 -0500

    🔥 Removed Stupid Comments

--- tests/tests.js
+++ tests/tests.js
@@ -1,10 +1,8 @@
 describe(`A simple test!`, () => {
     
-    expect(32 + 8).toBe(40); // passing
-
-    expect(20 * 40).toBeGreaterThan(10 * 40); // passing
-    
-    expect("New String").toExist().and().length().toBe(10); // passing
+    expect(32 + 8).toBe(40);
+    expect(20 * 40).toBeGreaterThan(10 * 40);
+    expect("New String").toExist().and().length().toBe(10);
 
 });
 
