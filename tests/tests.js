35c3aa9fb697c9006f88af41d23c786c2688b139
William McGonagle <wpmcgonagle@gmail.com>
2021-12-30 22:38:39 -0500

    🚑 Switched Test to Passing

--- tests/tests.js
+++ tests/tests.js
@@ -2,7 +2,7 @@ describe(`A simple test!`, () => {
     
     expect(32 + 8).toBe(40); // passing
 
-    expect(20 * 40).toBeGreaterThan(40 * 40); // failed
+    expect(20 * 40).toBeGreaterThan(10 * 40); // passing
     
     expect("New String").toExist().and().length().toBe(10); // passing
 
