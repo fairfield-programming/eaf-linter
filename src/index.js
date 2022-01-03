c6eb8e847e5338e83a009e863a6d8159bb1d19db
William McGonagle <wpmcgonagle@gmail.com>
2022-01-03 13:55:17 -0500

    ✨ Added Check to Run Tests

--- src/index.js
+++ src/index.js
@@ -5,5 +5,15 @@ const tester = require('./tester');
 const performer = require('./performer');
 
 reporter.storeMetricsFile();
-tester.testFolder(process.cwd());
+
+if (global.eafSettings["run-tests"] != undefined) {
+
+    if (global.eafSettings["run-tests"] != false)
+        tester.testFolder(process.cwd());
+
+} else {
+
+    tester.testFolder(process.cwd());
+
+}
 // performer.testFolder(process.cwd());
