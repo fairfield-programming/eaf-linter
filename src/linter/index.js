1675cbce9b27f18e402e0bbefba2697b44aee27d
William McGonagle <wpmcgonagle@gmail.com>
2022-01-14 20:33:33 -0500

    ⚰️ Moved Around Dependenciesf

--- src/linter/index.js
+++ src/linter/index.js
@@ -4,51 +4,13 @@ const fs = require('fs');
 const commentSeparator = require('./pipe/commentSeparator');
 const staleDetector = require('./pipe/staleDetector');
 
-var components = {
-    VariableDeclarator: require("./components/VariableDeclarator"),
-    VariableDeclaration: require("./components/VariableDeclaration"),
-    BinaryExpression: require("./components/BinaryExpression"),
-    Identifier: require("./components/Identifier"),
-    NumericLiteral: require("./components/NumericLiteral"),
-    CommentLine: require("./components/CommentLine"),
-    FunctionDeclaration: require("./components/FunctionDeclaration"),
-    BlockStatement: require("./components/BlockStatement"),
-    StringLiteral: require("./components/StringLiteral"),
-    ExpressionStatement: require("./components/ExpressionStatement"),
-    CallExpression: require("./components/CallExpression"),
-    MemberExpression: require("./components/MemberExpression"),
-    ArrowFunctionExpression: require("./components/ArrowFunctionExpression"),
-    FunctionExpression: require("./components/FunctionExpression"),
-    AssignmentExpression: require("./components/AssignmentExpression"),
-    ArrayExpression: require("./components/ArrayExpression"),
-    IfStatement: require("./components/IfStatement"),
-    BooleanLiteral: require("./components/BooleanLiteral"),
-    NewExpression: require("./components/NewExpression"),
-    ObjectExpression: require("./components/ObjectExpression"),
-    ObjectProperty: require("./components/ObjectProperty"),
-    ReturnStatement: require("./components/ReturnStatement"),
-    ObjectMethod: require("./components/ObjectMethod"),
-    TemplateLiteral: require("./components/TemplateLiteral"),
-    TemplateElement: require("./components/TemplateElement"),
-    SpreadElement: require("./components/SpreadElement"),
-    ObjectPattern: require("./components/ObjectPattern"),
-    ConditionalExpression: require("./components/ConditionalExpression")
-}; 
+function fileToAST(input) {
 
-// Standard Piping Function
-function parse(item) {
+    // Check that Input is Defined
+    if (input == undefined) return "";
 
-    if (item == null) throw new Error(`Item is null`);
-
-    if (item.type == undefined) throw new Error(`${item} does not have type.`);
-
-    var component = components[item.type];
-
-    if (component == null) throw new Error(`Type ${item.type}, does not exist.`);
-
-    return component.write(item, {
-        parse
-    });
+    // Babel Parse It
+    var syntaxTree = parser.parse(input).program.body;
 
 }
 
@@ -70,30 +32,6 @@ function pipe(input) {
 
     // Return the Output
     return output;
-
-}
-
-function lintString(input) {
-
-    // Check that Input is Defined
-    if (input == undefined) return "";
-
-    // Babel Parse It
-    var syntaxTree = parser.parse(input).program.body;
-    var finalSyntaxTree = pipe(syntaxTree);
-
-    // Create Output Data
-    var output = [];
-
-    // Go Through Syntax Tree
-    finalSyntaxTree.forEach(function (element) {
-
-        output.push(parse(element));
-
-    });
-
-    // Return Output
-    return output.join("\n");
 
 }
 
---
+++ src/prettier/index.js
@@ -0,0 +1,76 @@
+const linter = require("../linter/index");
+
+var components = {
+    VariableDeclarator: require("./components/VariableDeclarator"),
+    VariableDeclaration: require("./components/VariableDeclaration"),
+    BinaryExpression: require("./components/BinaryExpression"),
+    Identifier: require("./components/Identifier"),
+    NumericLiteral: require("./components/NumericLiteral"),
+    CommentLine: require("./components/CommentLine"),
+    FunctionDeclaration: require("./components/FunctionDeclaration"),
+    BlockStatement: require("./components/BlockStatement"),
+    StringLiteral: require("./components/StringLiteral"),
+    ExpressionStatement: require("./components/ExpressionStatement"),
+    CallExpression: require("./components/CallExpression"),
+    MemberExpression: require("./components/MemberExpression"),
+    ArrowFunctionExpression: require("./components/ArrowFunctionExpression"),
+    FunctionExpression: require("./components/FunctionExpression"),
+    AssignmentExpression: require("./components/AssignmentExpression"),
+    ArrayExpression: require("./components/ArrayExpression"),
+    IfStatement: require("./components/IfStatement"),
+    BooleanLiteral: require("./components/BooleanLiteral"),
+    NewExpression: require("./components/NewExpression"),
+    ObjectExpression: require("./components/ObjectExpression"),
+    ObjectProperty: require("./components/ObjectProperty"),
+    ReturnStatement: require("./components/ReturnStatement"),
+    ObjectMethod: require("./components/ObjectMethod"),
+    TemplateLiteral: require("./components/TemplateLiteral"),
+    TemplateElement: require("./components/TemplateElement"),
+    SpreadElement: require("./components/SpreadElement"),
+    ObjectPattern: require("./components/ObjectPattern"),
+    ConditionalExpression: require("./components/ConditionalExpression")
+}; 
+
+function parse(item) {
+
+    if (item == null) throw new Error(`Item is null`);
+
+    if (item.type == undefined) throw new Error(`${item} does not have type.`);
+
+    var component = components[item.type];
+
+    if (component == null) throw new Error(`Type ${item.type}, does not exist.`);
+
+    return component.write(item, {
+        parse
+    });
+
+}
+
+function stringifyAST(ast) {
+
+    // Create Output Data
+    var output = [];
+
+    // Go Through Syntax Tree
+    finalSyntaxTree.forEach(function (element) {
+
+        output.push(parse(element));
+
+    });
+
+    // Return Output
+    return output.join("\n");
+
+}
+
+function cleanFile(input) {
+
+    // Read AST and Pipe It 
+    var syntaxTree = linter.fileToAST(input);
+    var finalSyntaxTree = pipe(syntaxTree);
+
+    // Return the Stringified AST
+    return stringifyAST(finalSyntaxTree);
+
+}
